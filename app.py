a4b1eac60a3979434892ca9f3cb6e37cde77e84c
Andy324 <andy.almueti1@gmail.com>
2017-06-15 04:52:14 +0000

    Fixed syntax errors

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-        return "Welcome!"
+    return 'Hello world'
 
 if __name__ == "__main__":
-        app.run(debug=True, host='0.0.0.0')
+    app.run()
+
+
+
+
+
+
+
+
+
+
+
+
 
 
