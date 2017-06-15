e44d804fb21275c29fb20c04a96b40b801e7de0f
Andy324 <andy.almueti1@gmail.com>
2017-06-15 04:58:53 +0000

    PhilsEdits v2

--- app.py
+++ app.py
@@ -1,25 +1,9 @@
-
 from flask import Flask
-
 app = Flask(__name__)
-
-@app.route('/')
+@app.route("/")
 def main():
-    return 'Hello world'
+    return "Hello world"
 
 if __name__ == "__main__":
     app.run()
-
-
-
-
-
-
-
-
-
-
-
-
-
 
