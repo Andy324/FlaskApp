8279b72fd5ac9d44e938c8cd4dc7d0070d04cc12
Andy324 <andy.almueti1@gmail.com>
2017-06-28 02:46:03 +0000

    because I said so

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def main():
 	return render_template('index.html')
-
+@app.route('/showSignUp')
+def showSignUp():
+	return render_template('signup.html')
 if __name__ == "__main__":
 
         app.run(debug=True, host='192.168.11.9')
